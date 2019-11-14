26d74ae534ffad08a980a3b7c547f7a2d9781f45
Matthew Wardle <mwardle.22@gmail.com>
2019-11-14 01:13:58 +0000

    Set up students.js and teachers.js, using node to make SQL queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+const args = process.argv.slice(2);
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers on teachers.id = teacher_id
+JOIN students on students.id = student_id
+JOIN cohorts on cohorts.id = cohort_id
+WHERE cohorts.name = '${args[0] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
+  })
+  .catch(err => console.error('query error', err.stack));
